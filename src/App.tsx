eec3e116417a798ae5f073a38aa5bd600943c706
aazadpanchhi05 <darshiljadav12@gmail.com>
2021-09-09 17:21:29 +0530

    hide header in signup and login

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import './App.css'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  withRouter,
+} from 'react-router-dom'
 import SignUp from './Pages/SignUp'
 import Login from './Pages/Login'
 import Setting from './Pages/Setting'
@@ -10,17 +15,29 @@ import Home from './Pages/Home'
 import { Header } from './Components/Header'
 
 function App() {
+  const Main = withRouter(({ location }) => {
+    return (
+      <>
+        {location.pathname !== '/login' && location.pathname !== '/signup' && (
+          <Header />
+        )}
+        <main>
+          <Switch>
+            <Route path="/settings" component={Setting} />
+            <Route path="/profile" component={Profile} />
+            <Route path="/user-list" component={UserList} />
+            <Route path="/login" component={Login} />
+            <Route path="/signup" component={SignUp} />
+            <Route path="/" component={Home} exact />
+          </Switch>
+        </main>
+      </>
+    )
+  })
+
   return (
     <Router>
-      <Header />
-      <Switch>
-        <Route path="/settings" component={Setting} />
-        <Route path="/profile" component={Profile} />
-        <Route path="/user-list" component={UserList} />
-        <Route path="/login" component={Login} />
-        <Route path="/signup" component={SignUp} />
-        <Route path="/" component={Home} exact />
-      </Switch>
+      <Main />
     </Router>
   )
 }
