98891e16ca1cf505fc8a4eb2254637fd732a139f
aazadpanchhi05 <darshiljadav12@gmail.com>
2021-09-16 10:11:24 +0530

    Changes in icon size

--- src/Components/Sidebar/Sidebar.tsx
+++ src/Components/Sidebar/Sidebar.tsx
@@ -6,7 +6,7 @@ import { SidebarData } from './SidebarData'
 export const Sidebar = () => {
   const classes = {
     sidebar: 'bg-white flex-shrink-0 min-h-screen w-64 border-r-2',
-    nav: 'flex flex-row space-x-5  p-3 text-black hover:bg-indigo-400 hover:text-white',
+    nav: 'flex flex-row space-x-5  p-3 text-black hover:bg-indigo-200',
     navActive: 'flex text-white bg-indigo-500',
   }
 
@@ -20,7 +20,7 @@ export const Sidebar = () => {
             activeClassName={classes.navActive}
             exact
           >
-            <i className="mt-1">{item.icon}</i>
+            <div className="mt-1 ml-2 text-xl">{item.icon}</div>
             <div>{item.title}</div>
           </NavLink>
         ))}
